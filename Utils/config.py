727a859831991fae45bd6dd91d7bee7effc025b5
Gabriel Gausachs <g4a4b4r4i@gmail.com>
2024-11-02 11:35:49 +0100

    jkjlkj

--- Utils/config.py
+++ Utils/config.py
@@ -7,7 +7,7 @@ import os
 INPUT_FOLDER = os.path.join(
     os.path.dirname(os.path.dirname((__file__))), "football_videos"
 )
-INPUT_NAME_VIDEO = "video_example1.mp4"
+INPUT_NAME_VIDEO ="video_example1.mp4"
 OUTPUT_FOLDER = os.path.join(
     os.path.dirname(os.path.dirname((__file__))), "football_videos/output"
 )
